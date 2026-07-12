94df1975a2d79ac688db090006c7c37a98d51b4e
Rupak Chandra Das <rupakcdp48@gmail.com>
2026-07-12 12:23:47 +0600

    Update fraud_detect.sql

--- Scripts/fraud_detect.sql
+++ Scripts/fraud_detect.sql
@@ -41,3 +41,41 @@ FROM velocity
 WHERE txns_in_10min > 5
 GROUP BY account_id , txn_at
 ORDER BY max_txns DESC , txn_at DESC;
+
+
+
+-- ── FRAUD RULE 2: Geographic anomaly — transaction outside home country ───────
+-- Real-world: Account always transacts in US, suddenly appears in Romania
+
+with mode_country as (
+		SELECT account_id, 
+        merchant_country, 
+        COUNT(merchant_country) AS txn_per_cntry,
+		RANK() OVER ( PARTITION BY account_id ORDER BY COUNT(merchant_country) DESC) AS rn
+		FROM transactions
+		GROUP BY account_id,  merchant_country
+		ORDER BY account_id asc ),
+home_country AS (
+    SELECT
+        account_id,
+		merchant_country AS home_cntry,
+        txn_per_cntry
+    FROM mode_country
+    WHERE rn = 1 AND  txn_per_cntry >= 2  -- (found a baseline transaction count from history)
+    ),
+anomalies AS (
+    SELECT
+        t.account_id,
+        t.txn_id,
+        t.merchant,
+        t.merchant_country,
+        t.amount,
+        t.txn_at,
+        hc.home_cntry
+    FROM transactions t
+    INNER JOIN home_country hc ON t.account_id = hc.account_id
+    WHERE t.merchant_country <> hc.home_cntry
+)
+SELECT * FROM anomalies ORDER BY amount DESC LIMIT 50;
+
+
